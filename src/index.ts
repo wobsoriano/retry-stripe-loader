235685077d321f05bd1fabbaa3c0e7bd3124fd5d
wobsoriano <sorianorobertc@gmail.com>
2024-08-25 16:25:49 -0700

    chore: Bump all non-major deps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import type { StripeConstructorOptions } from '@stripe/stripe-js'
-import { loadStripe as _loadStripe } from '@stripe/stripe-js'
-import type { Options as RetryOptions } from 'p-retry'
-import pRetry from 'p-retry'
+import type { StripeConstructorOptions } from '@stripe/stripe-js';
+import { loadStripe as _loadStripe } from '@stripe/stripe-js';
+import type { Options as RetryOptions } from 'p-retry';
+import pRetry from 'p-retry';
 
 export const loadStripe = (
-  publishableKey: string,
-  options?: StripeConstructorOptions & RetryOptions,
-) => pRetry(() => _loadStripe(publishableKey, options), options)
+	publishableKey: string,
+	options?: StripeConstructorOptions & RetryOptions,
+) => pRetry(() => _loadStripe(publishableKey, options), options);
